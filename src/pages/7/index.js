28660002bf234da42a8219bb2a08a5364adb2b71
Erik Hollensbe <git@hollensbe.org>
2022-05-28 21:31:58 -0700

    fixed all of the things
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/pages/7/index.js
+++ src/pages/7/index.js
@@ -61,64 +61,62 @@ class Page7 extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h4">More Widgets</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1">
-              Here are some more widgets you may encounter in your travels!
-            </Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h5">Alerts:</Typography>
-            <br />
-            <Alert severity="error">
-              <AlertTitle>Error</AlertTitle>
-              This is an error
-            </Alert>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h5">Dialogs:</Typography>
-            <br />
-            <Button variant="outlined" onClick={() => { this.handleOpen() }}>
-              Click me: {this.state.email}
-            </Button>
-            <Dialog onClose={() => { this.handleClose() }} open={this.state.opened}>
-              <DialogTitle>Set backup account</DialogTitle>
-              <List sx={{ pt: 0 }}>
-                {EMAILS.map((email) => (
-                  <ListItem button onClick={() => this.handleListItemClick(email)} key={email}>
-                    <ListItemAvatar>
-                      <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                        <PersonIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={email} />
-                  </ListItem>
-                ))}
-              </List>
-            </Dialog>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h5">Progress Bar:</Typography>
-            <br />
-            <LinearProgress variant="determinate" value={80} /> 
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4">More Widgets</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1">
+            Here are some more widgets you may encounter in your travels!
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h5">Alerts:</Typography>
+          <br />
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            This is an error
+          </Alert>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h5">Dialogs:</Typography>
+          <br />
+          <Button variant="outlined" onClick={() => { this.handleOpen() }}>
+            Click me: {this.state.email}
+          </Button>
+          <Dialog onClose={() => { this.handleClose() }} open={this.state.opened}>
+            <DialogTitle>Set backup account</DialogTitle>
+            <List sx={{ pt: 0 }}>
+              {EMAILS.map((email) => (
+                <ListItem button onClick={() => this.handleListItemClick(email)} key={email}>
+                  <ListItemAvatar>
+                    <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                      <PersonIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={email} />
+                </ListItem>
+              ))}
+            </List>
+          </Dialog>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h5">Progress Bar:</Typography>
+          <br />
+          <LinearProgress variant="determinate" value={80} /> 
         </Grid>
         <Grid item xs={4}>
           <Typography variant="h5">Speed Dials:</Typography>
-            <SpeedDial
-              ariaLabel="SpeedDial basic example"
-              icon={<SpeedDialIcon />}
-            >
-              <SpeedDialAction
-                key="copy"
-                icon={<CopyAllIcon />}
-                tooltipTitle="copy"
-              />
-            </SpeedDial>
+          <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            icon={<SpeedDialIcon />}
+          >
+            <SpeedDialAction
+              key="copy"
+              icon={<CopyAllIcon />}
+              tooltipTitle="copy"
+            />
+          </SpeedDial>
         </Grid>
         <Grid item xs={4}>
           <Typography variant="h5">Cards:</Typography>
@@ -219,7 +217,7 @@ class Page7 extends React.Component {
             </AccordionDetails>
           </Accordion>
         </Grid>
-      </React.Fragment>
+      </Grid>
     );
   };
 }
