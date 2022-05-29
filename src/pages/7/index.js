91fdc1e0be42edddc99bf11e63f1883349dda8c4
Erik Hollensbe <git@hollensbe.org>
2022-05-28 19:24:20 -0700

    accordions
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/pages/7/index.js
+++ src/pages/7/index.js
@@ -21,8 +21,12 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
 
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import PersonIcon from '@mui/icons-material/Person';
 import CopyAllIcon from '@mui/icons-material/CopyAll';
 
@@ -33,7 +37,15 @@ const EMAILS = [
 ]
 
 class Page7 extends React.Component {
-  state = { opened: false, email: EMAILS[0] }
+  state = { opened: false, email: EMAILS[0], expanded: null }
+
+  handleAccordionChange(param) {
+    if (this.state.expanded === param) {
+      this.setState({ expanded: null });
+    } else {
+      this.setState({ expanded: param });
+    }
+  }
 
   handleListItemClick(email) {
     this.setState({ email, opened: false })
@@ -131,6 +143,81 @@ class Page7 extends React.Component {
               <Button size="small">Learn More</Button>
             </CardActions>
           </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h5">Accordions!</Typography>
+          <Accordion expanded={this.state.expanded === 'panel1'} onChange={() => this.handleAccordionChange('panel1')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                General settings
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={this.state.expanded === 'panel2'} onChange={() => this.handleAccordionChange('panel2')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                You are currently not an owner
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+                varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+                laoreet.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={this.state.expanded === 'panel3'} onChange={() => this.handleAccordionChange('panel3')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                Advanced settings
+              </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                Filtering has been entirely disabled for whole web server
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                amet egestas eros, vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={this.state.expanded === 'panel4'} onChange={() => this.handleAccordionChange('panel4')}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+                amet egestas eros, vitae egestas augue. Duis vel est augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </Grid>
       </React.Fragment>
     );
